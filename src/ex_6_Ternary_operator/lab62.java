3528af034c51312ac1f3ddc1f4406866cdfa5d4c
Srujan0423 <Saisrujan0423@gmail.com>
2025-06-14 12:38:21 +0530

    First Commit

---
+++ src/ex_6_Ternary_operator/lab62.java
@@ -0,0 +1,9 @@
+package ex_6_Ternary_operator;
+
+public class lab62 {
+    public static void main(String[] args) {
+        int num=5;
+        String s=num>0? "positive":"neg";
+        System.out.println(s);
+    }
+}
