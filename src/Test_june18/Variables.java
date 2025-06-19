4fbd7d9272cc8b2b895e88b82919266879e19d46
Srujan0423 <saisrujan0423@gmail.com>
2025-06-19 08:07:37 +0530

    first question variables

---
+++ src/Test_june18/Variables.java
@@ -0,0 +1,17 @@
+package Test_june18;
+
+public class Variables  {
+
+       static int i;
+        static float j;
+        static boolean k;
+        static double l;
+    public static void main(String[] args) {
+        System.out.println(k);
+        System.out.println(j);
+        System.out.println(i);
+        System.out.println(l);
+
+
+    }
+}
