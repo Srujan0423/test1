e812b0979dab9675e2b77e1efdeeddba781dbbff
Srujan0423 <saisrujan0423@gmail.com>
2025-06-19 08:23:09 +0530

    compare two numbers

---
+++ src/Test_june18/Compare_two_Numbers.java
@@ -0,0 +1,12 @@
+package Test_june18;
+
+public class Compare_two_Numbers {
+
+
+    public static void main(String[] args) {
+        int i=4; float g=5f; double d=7;
+        System.out.println(i==g);
+        System.out.println(i<=d);
+        System.out.println(i=i++);
+    }
+}
