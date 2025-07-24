614bc71f5dbb074aea978511240bda08a021ad31
Srujan0423 <Saisrujan0423@gmail.com>
2025-07-24 13:07:20 +0530

    oops concepts

---
+++ src/OOPS/ENCAPSULATION_PROG1.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+public class ENCAPSULATION_PROG1 {
+
+     String name;
+     int height;
+     String Gender;
+     int age;
+     boolean is_married;
+
+     void walk(){
+         System.out.println("walking");
+     }
+     void  eat(){
+         System.out.println("eat");
+     }
+     void sleep(){
+         System.out.println("sleep");
+     }
+
+
+}
